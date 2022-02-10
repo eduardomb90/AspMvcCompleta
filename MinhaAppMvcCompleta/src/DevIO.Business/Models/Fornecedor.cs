73457758a55e1471e01d77be5af08659c6f72b63
eduardomb90 <eduardowemb@gmail.com>
2022-02-10 18:19:50 -0300

    Iniciando app completa

---
+++ MinhaAppMvcCompleta/src/DevIO.Business/Models/Fornecedor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppMvcBasica.Models
+{
+    public class Fornecedor : Entity
+    {
+        public string Nome { get; set; }
+        public string Documento { get; set; }
+        public TipoFornecedor TipoFornecedor { get; set; }
+        public Endereco Endereco { get; set; }
+        public bool Ativo { get; set; }
+
+        /* EF Relations */
+        // A lista de produtos não necessitaria estar aqui, mas para fins de relacionamento no EF, é necessário
+
+        public IEnumerable<Produto> Produtos { get; set; } //Fornecedor tem uma relação de 1 para N com Produtos
+    }
+}
---
+++ MinhaAppMvcCompleta/src/DevIO.Business/Models/Produto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppMvcBasica.Models
+{
+    public class Produto : Entity
+    {
+        /*Em uma relação 1 para N, a chave estrangeira fica em N*/
+        public Guid FornecedorId { get; set; } //Isto bastaria para um banco de dados, mas não para o EF
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public string Imagem { get; set; }
+        public decimal Valor { get; set; }
+        public DateTime DataCadastro { get; set; }
+        public bool Ativo { get; set; }
+
+        /*EF Relations, é preciso colocar um campo fornecedor (além da foreignKey) para o EF*/
+        public Fornecedor Fornecedor { get; set; } //Esta é uma propriedade de navegação do EF
+    }
+}
